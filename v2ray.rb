0ced1d081b097bc998196d1c8299c580accf25ce
pandada8 <pandada8@gmail.com>
2017-04-17 10:59:55 +0800

    bump

--- v2ray.rb
+++ v2ray.rb
@@ -1,7 +1,7 @@
 class V2ray < Formula
   desc "V2Ray provides building blocks for network proxy development."
   homepage "https://github.com/v2ray/v2ray-core"
-  version "2.20.1"
+  version "2.23"
   url "https://github.com/v2ray/v2ray-core/releases/download/v#{version}/v2ray-macos.zip"
 
   def plist; <<-EOS.undent
